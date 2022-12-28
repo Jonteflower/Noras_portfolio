590c931ff11e0f7d12640ce43b4afead54db6d40
Jonathan Nairn Tallen <jonathannairn@hotmail.com>
2022-12-28 13:12:27 +0100

    changes

--- components/home.js
+++ components/home.js
@@ -2,17 +2,25 @@ import React, { useRef } from 'react'
 import HeroSection from './heroSection'
 import SkillsSection from './skillsSection'
 import dynamic from 'next/dynamic'
+import { Suspense } from "react";
+
 
 const DynamicExperienceSection = dynamic(() => import('./experienceSection'), {
     ssr: false,
+    suspense: true,
+
 })
 
 const DynamicProjectsSection = dynamic(() => import('./founderSection'), {
     ssr: false,
+    suspense: true,
+
 })
 
 const DynamicContactSection = dynamic(() => import('./contactSection'), {
     ssr: false,
+    suspense: true,
+
 })
 
 
