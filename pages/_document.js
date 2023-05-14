ee086d6a65712e125611a4ef4b59de5aeb416764
Jonathan Nairn Tallen <jonathannairn@hotmail.com>
2023-05-14 11:57:39 +0200

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -39,7 +39,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk:ital@1&family=Inter:wght@300;400;500;600&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
             rel="stylesheet"
           />
         </Head>
